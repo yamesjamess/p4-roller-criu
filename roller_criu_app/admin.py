7dca5a408485d76530322948112f5ea3331fcfc0
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-02 17:19:12 +0000

    Add summernotes, filters, and displays to Admin panel

--- roller_criu_app/admin.py
+++ roller_criu_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Class, Feedback
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Class)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+
+
+@admin.register(Feedback)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'class_id', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'user_id', 'body']
+    actions = ['approved_comments']
+
+    def approved_comments(self, request, queryset):
+        queryset.update(approved=True)
