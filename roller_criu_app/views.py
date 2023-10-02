3ec11500c5c611a4a57ee3c50884ce9179b0bd85
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-02 20:45:45 +0000

    Linking up the views.py and urls.py

--- roller_criu_app/views.py
+++ roller_criu_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Class
 
-# Create your views here.
+
+class ClassList(generic.ListView):
+    model = Class
+    queryset = Class.objects.filter(status=1).order_by('class_level')
+    template_name = 'index.html'
+    paginate_by = 6
+
--- roller_criu_project/urls.py
+++ roller_criu_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls'))
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('roller_criu_app.urls'), name='roller_criu_urls'),
 ]
