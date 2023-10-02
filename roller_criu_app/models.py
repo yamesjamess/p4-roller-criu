9e6ba61bd83505ec7bd5fe28885b41c5b2590612
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-02 17:19:37 +0000

    Create models for Class and Feedback

--- roller_criu_app/models.py
+++ roller_criu_app/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Class(models.Model):
+    # code from https://adamj.eu/tech/2020/01/27/moving-to-django-3-field-choices-enumeration-types/
+    BEGINNER = 'beginner'
+    INTERMEDIATE = 'intermediate'
+    ADVANCED = 'advanced'
+
+    LEVEL_CHOICES = [
+        (BEGINNER, 'Beginner'),
+        (INTERMEDIATE, 'Intermediate'),
+        (ADVANCED, 'Advanced'),
+    ]
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    class_time = models.DateTimeField()
+    class_level = models.CharField(max_length=20, choices=LEVEL_CHOICES, default=BEGINNER)
+    location = models.CharField(max_length=100)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholer')
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='class_likes', blank=True)
+    # coach = models.ForeignKey(Coach, on_delete=models.SET_DEFAULT, default=1, related_name='class_post')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['class_level']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Feedback(models.Model):
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='feedbacks')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='feedbacks')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.name}'
