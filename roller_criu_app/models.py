c6b591571d1205e8df502cb3df4517f435fedba9
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-03 21:57:57 +0000

    Create Coach model

--- roller_criu_app/models.py
+++ roller_criu_app/models.py
@@ -5,6 +5,35 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+class Coach(models.Model):
+    RECREATIONAL = 'recreational'
+    FREESTYLE = 'freestyle'
+    DANCE = 'dance'
+    PAIRS = 'pairs'
+
+    SPECIALIZATION_CHOICES = [
+        (RECREATIONAL, 'Recreational'),
+        (FREESTYLE, 'Freestyle'),
+        (DANCE, 'Dance'),
+        (PAIRS, 'Pairs'),
+    ]
+
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=100)
+    bio = models.CharField(max_length=140, help_text="Enter a brief bio")
+    image = CloudinaryField('image', default='placeholder')
+    specialization = models.CharField(max_length=20, choices=SPECIALIZATION_CHOICES, default=RECREATIONAL)
+    years_of_experience = models.PositiveIntegerField(default='1', help_text="Enter a positive integer.")
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['pk']
+
+    def __str__(self):
+        return f'Coach {self.first_name} {self.last_name}'
+
+
 class Lesson(models.Model):
     # code from https://adamj.eu/tech/2020/01/27/moving-to-django-3-field-choices-enumeration-types/
     BEGINNER = 'beginner'
@@ -26,12 +55,12 @@ class Lesson(models.Model):
     featured_image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='lesson_likes', blank=True)
-    # coach = models.ForeignKey(Coach, on_delete=models.SET_DEFAULT, default=1, related_name='lesson_post')
+    coach = models.ForeignKey(Coach, on_delete=models.SET(1), related_name='lesson_coach')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['lesson_level']
+        ordering = ['lesson_time']
 
     def __str__(self):
         return self.title
