e974a059e948d8fb70adc80be9af2b77b291ae9d
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-03 18:04:34 +0000

    Linking the LessonDetail view to the urls file

--- roller_criu_app/urls.py
+++ roller_criu_app/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.LessonList.as_view(), name='home')
+    path('', views.LessonList.as_view(), name='home'),
+    path('<slug:slug>/', views.LessonDetail.as_view(), name='lesson_detail'),
 ]
