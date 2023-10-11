c1608beec9d29dd12744f1682b5536ded71d6069
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-11 22:02:29 +0000

    Add LessonLike view to urls file

--- roller_criu_app/urls.py
+++ roller_criu_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.LessonList.as_view(), name='home'),
     path('<slug:slug>/', views.LessonDetail.as_view(), name='lesson_detail'),
+    path('like/<slug:slug>/', views.LessonLike.as_view(), name='lesson_like'),
 ]
