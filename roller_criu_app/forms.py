16f8ee6a8358de66d4c1eddb8fd08b7fa6e58857
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-18 11:27:05 +0000

    Create ContactForm in the forms.py to handle Contact form

--- roller_criu_app/forms.py
+++ roller_criu_app/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Feedback
+from .models import Feedback, Contact
 
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ('body',)
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'contact_message')
