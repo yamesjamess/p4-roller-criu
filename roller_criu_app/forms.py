bc043c7e6e5b57b9003ff071761d0c8afb4a871e
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-23 00:26:55 +0000

    Create BookingForm to handle user bookings

--- roller_criu_app/forms.py
+++ roller_criu_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Feedback, Contact
+from .models import Feedback, Contact, Booking
 
 
 class FeedbackForm(forms.ModelForm):
@@ -12,3 +12,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('name', 'email', 'contact_message')
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ('places_reserved',)
