ce56a99ef600c15ed79950ccec038e6699904127
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-23 22:20:28 +0000

    Linking MyBooking view to the url file

--- roller_criu_app/urls.py
+++ roller_criu_app/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('about/', views.About.as_view(), name='about'),
     path('contact/', views.Contact.as_view(), name='contact'),
     path('contact/success/', TemplateView.as_view(template_name='contact_success.html'), name='contact_success'),
+    path('my_bookings/', views.MyBookings.as_view(), name='my_bookings'),
     path('<slug:slug>/', views.LessonDetail.as_view(), name='lesson_detail'),
     path('like/<slug:slug>/', login_required(views.LessonLike.as_view()), name='lesson_like'),
+    
 ]
