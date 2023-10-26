4bd90f6dce979220b22bff990fc232847cb9cd8b
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-26 20:26:53 +0000

    Add view and url for handling error 403, 404, 405, and 500

--- roller_criu_project/urls.py
+++ roller_criu_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
+handler404 = 'roller_criu_project.views.handler404'
+handler500 = 'roller_criu_project.views.handler500'
+handler403 = 'roller_criu_project.views.handler403'
+handler405 = 'roller_criu_project.views.handler405'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
