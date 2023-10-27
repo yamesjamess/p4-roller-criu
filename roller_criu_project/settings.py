226c199da7b1dbcfeb0aeca68f3bf7b344f3e518
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-27 22:19:42 +0000

    Change the allowed host to the new gitpod workspace url and set debug to true for testing

--- roller_criu_project/settings.py
+++ roller_criu_project/settings.py
@@ -32,9 +32,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-yamesjamess-p4rollercri-yt5w1843zvz.ws-eu105.gitpod.io', 'roller-criu-54a9c256735c.herokuapp.com']
+ALLOWED_HOSTS = ['8000-yamesjamess-p4rollercri-v4meaprc37s.ws-eu105.gitpod.io', 'roller-criu-54a9c256735c.herokuapp.com']
 
 
 # Application definition
@@ -109,16 +109,16 @@ WSGI_APPLICATION = 'roller_criu_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 
 # Password validation
