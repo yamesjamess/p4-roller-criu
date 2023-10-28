8f70f1616ede62b95da8797f1a6acdfac5886c69
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-28 22:20:03 +0000

    Create setUpTestData from all the models to be used in testing the views

--- roller_criu_app/test_views.py
+++ roller_criu_app/test_views.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+import pytz
+from .models import Coach, Lesson, Feedback, Booking, Contact
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create(username='testuser')
+        self.user.set_password('12345')
+        self.user.save()
+
+        self.coach = Coach.objects.create(
+            first_name='John',
+            last_name='Smith',
+            email='john@rollercriu.com',
+            bio='This is a test bio',
+            specialization=Coach.RECREATIONAL,
+            years_of_experience=5,
+            status=1
+        )
+
+        self.lesson = Lesson.objects.create(
+            title='Test Lesson 1',
+            slug='test-lesson-1',
+            lesson_start=datetime(2018, 4, 4, 0, 0, 0, tzinfo=pytz.utc),
+            lesson_level=Lesson.BEGINNER,
+            duration=timedelta(hours=1),
+            location='Test Location 1',
+            coach=self.coach,
+            content='Test content for lesson 1',
+            status=0
+        )
+
+        self.feedback = Feedback.objects.create(
+            lesson=self.lesson,
+            username=self.user,
+            email='user@email.com',
+            body='Feedback 2',
+            approved=True
+        )
+
+        self.booking = Booking.objects.create(
+            lesson=self.lesson,
+            username=self.user,
+            places_reserved=1,
+            approved='approved'
+        )
+
+        self.contact = Contact.objects.create(
+            name='Jane',
+            email='jane@email.com',
+            contact_message='This is a test contact message'
+        )
+
+
