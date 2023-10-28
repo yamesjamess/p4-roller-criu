be311a467785338bc575bcae55d003367beae20a
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-28 23:15:36 +0000

    Create test case to check if user can like and unlike a lesson

--- roller_criu_app/test_views.py
+++ roller_criu_app/test_views.py
@@ -10,14 +10,13 @@ class TestViews(TestCase):
 
     # instantiate coach, lesson, feedback, booking, and contact
     @classmethod
-    def setUpTestData(self):
-        self.user = User.objects.create_user(
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
             username='testuser',
             password='12345'
             )
-        self.user.save()
 
-        self.coach = Coach.objects.create(
+        cls.coach = Coach.objects.create(
             first_name='John',
             last_name='Smith',
             email='john@rollercriu.com',
@@ -27,34 +26,34 @@ class TestViews(TestCase):
             status=1
         )
 
-        self.lesson = Lesson.objects.create(
+        cls.lesson = Lesson.objects.create(
             title='Test Lesson 1',
             slug='test-lesson-1',
             lesson_start=datetime(2018, 4, 4, 0, 0, 0, tzinfo=pytz.utc),
             lesson_level=Lesson.BEGINNER,
             duration=timedelta(hours=1),
             location='Test Location 1',
-            coach=self.coach,
+            coach=cls.coach,
             content='Test content for lesson 1',
             status=0
         )
 
-        self.feedback = Feedback.objects.create(
-            lesson=self.lesson,
-            username=self.user,
+        cls.feedback = Feedback.objects.create(
+            lesson=cls.lesson,
+            username=cls.user,
             email='user@email.com',
             body='Feedback 2',
             approved=True
         )
 
-        self.booking = Booking.objects.create(
-            lesson=self.lesson,
-            username=self.user,
+        cls.booking = Booking.objects.create(
+            lesson=cls.lesson,
+            username=cls.user,
             places_reserved=1,
             approved='approved'
         )
 
-        self.contact = Contact.objects.create(
+        cls.contact = Contact.objects.create(
             name='Jane',
             email='jane@email.com',
             contact_message='This is a test contact message'
@@ -89,9 +88,32 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'base.html')
         self.assertTemplateUsed(response, 'my_bookings.html')
 
-    # # get lesson detail page and check if correct templates are being used
+    # get lesson detail page and check if correct templates are being used
     def test_get_lesson_detail_page(self):
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'base.html')
         self.assertTemplateUsed(response, 'about.html')
+
+    # check if the user can toggle like and unlike
+    def test_can_toggle_like_lesson(self):
+        self.client.login(username='testuser', password='12345')
+
+        lesson_detail_url = reverse('lesson_detail', kwargs={'slug': 'test-lesson-1'})
+        response = self.client.get(lesson_detail_url)
+
+        numlikes = self.lesson.number_of_likes()
+        self.assertEqual(numlikes, 0)
+
+        like_url = reverse('lesson_like', kwargs={'slug': 'test-lesson-1'})
+        response = self.client.post(like_url)
+
+        self.assertEqual(response.status_code, 302)
+        self.lesson.refresh_from_db()
+        self.assertEqual(self.lesson.number_of_likes(), 1)
+
+        response = self.client.post(like_url)
+
+        self.assertEqual(response.status_code, 302)
+        self.lesson.refresh_from_db()
+        self.assertEqual(self.lesson.number_of_likes(), 0)
