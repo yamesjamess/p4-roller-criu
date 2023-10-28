fac060287009d7fd9ac28cf4505327b0669cdfc1
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-28 14:06:13 +0000

    Add second test for feedback form to test validity of form fields

--- roller_criu_app/test_forms.py
+++ roller_criu_app/test_forms.py
@@ -6,8 +6,13 @@ class TestFeedbackForm(TestCase):
 
     # test to check whether if a feedback is provided or not
     def test_feedback_form_is_required(self):
-        form = FeedbackForm({'body': ''})
+        form_data = {'body': ''}
+        form = FeedbackForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn('body', form.errors.keys())
         self.assertEqual(form.errors['body'][0], 'This field is required.')
     
+    # test to check if form fields are set up correctly
+    def test_fields_in_form(self):
+        form = FeedbackForm()
+        self.assertEqual(form.Meta.fields, ('body',))
