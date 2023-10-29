9b5eedc3deb71eddfdbbe097db75713dccd51b3e
yamesjamess <jameskasetarapanya15@gmail.com>
2023-10-29 18:33:23 +0000

    Create mock database for testing the models

--- roller_criu_app/test_models.py
+++ roller_criu_app/test_models.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Coach, Lesson, Feedback, Booking, Contact
+
+
+class TestModels(TestCase):
+
+    # instantiate coach, lesson, feedback, booking, and contact
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='12345',
+            email='test@email.com'
+            )
+
+        self.coach = Coach.objects.create(
+            first_name='John',
+            last_name='Smith',
+            email='john@rollercriu.com',
+            bio='This is a test bio',
+            specialization=Coach.RECREATIONAL,
+            years_of_experience=5,
+            status=1
+        )
+
+        self.lesson = Lesson.objects.create(
+            title='Test Lesson 1',
+            slug='test-lesson-1',
+            lesson_start=datetime(2018, 4, 4, 0, 0, 0, tzinfo=pytz.utc),
+            lesson_level=Lesson.BEGINNER,
+            duration=timedelta(hours=1),
+            location='Test Location 1',
+            coach=self.coach,
+            content='Test content for lesson 1',
+            status=0
+        )
+
+        self.feedback = Feedback.objects.create(
+            lesson=self.lesson,
+            username=self.user,
+            email='user@email.com',
+            body='Feedback 2',
+            approved=True
+        )
+
+        self.booking = Booking.objects.create(
+            lesson=self.lesson,
+            username=self.user,
+            places_reserved=1,
+            approved='approved'
+        )
+
+        self.contact = Contact.objects.create(
+            name='Jane',
+            email='jane@email.com',
+            contact_message='This is a test contact message'
+        )
+
+    
